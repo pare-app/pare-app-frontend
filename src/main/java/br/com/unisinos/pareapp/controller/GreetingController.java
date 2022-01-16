f81496a9d785391678729a3d5e7afae9e2f670a6
Rafael Correa Alves <rafael.correa@dbccompany.com.br>
2022-01-16 15:53:14 -0300

    Added test with heroku url

--- src/main/java/br/com/unisinos/pareapp/controller/GreetingController.java
+++ src/main/java/br/com/unisinos/pareapp/controller/GreetingController.java
@@ -29,7 +29,7 @@ public class GreetingController {
     public String greetingSubmit(@ModelAttribute(name = "greeting") Greeting greeting, Model model) {
         try {
             Greeting result = restTemplate.postForObject(
-                    "http://localhost:8080/greeting?id=" + greeting.getId() + "&message=" + greeting.getContent(),
+                    "https://pare-app-backend.herokuapp.com/greeting?id=" + greeting.getId() + "&message=" + greeting.getContent(),
                     null, Greeting.class);
             log.info(String.format("sent - %s", greeting != null ? greeting.toString() : "NULL"));
             log.info(String.format("received - %s", result != null ? result.toString() : "NULL"));
