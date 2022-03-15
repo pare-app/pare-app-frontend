53c7b2169eaa06d0c169e347cc409746af73643b
Rafael Correa Alves <rafael.correa@dbccompany.com.br>
2022-03-15 15:14:39 -0300

    Added board by questions linked to session page

--- src/main/java/br/com/unisinos/pareapp/controller/SessionPageController.java
+++ src/main/java/br/com/unisinos/pareapp/controller/SessionPageController.java
@@ -1,13 +1,7 @@
 package br.com.unisinos.pareapp.controller;
 
-import br.com.unisinos.pareapp.model.dto.entity.ExerciseDto;
-import br.com.unisinos.pareapp.model.dto.entity.PairDto;
-import br.com.unisinos.pareapp.model.dto.entity.SessionDto;
-import br.com.unisinos.pareapp.model.dto.entity.UserDto;
-import br.com.unisinos.pareapp.service.ExerciseService;
-import br.com.unisinos.pareapp.service.HttpSessionService;
-import br.com.unisinos.pareapp.service.PairService;
-import br.com.unisinos.pareapp.service.SessionService;
+import br.com.unisinos.pareapp.model.dto.entity.*;
+import br.com.unisinos.pareapp.service.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -16,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -24,21 +21,42 @@ import org.springframework.web.servlet.ModelAndView;
 public class SessionPageController extends BaseController {
     private final SessionService sessionService;
     private final ExerciseService exerciseService;
+    private final QuestionService questionService;
     private final PairService pairService;
 
     @GetMapping({"/{exerciseId}/{pairId}"})
     public ModelAndView session(Model model,
                                 @PathVariable(name = "exerciseId") Integer exerciseId,
                                 @PathVariable(name = "pairId") Integer pairId) {
+        ExerciseDto exerciseDto = exerciseService.get(exerciseId);
+        QuestionDto firstQuestionDto = exerciseDto.getQuestions().stream().findFirst().orElse(new QuestionDto());
+
+        return getView(model,"redirect:" + gerRedirectionPath(exerciseId, pairId, firstQuestionDto));
+    }
+
+    private String gerRedirectionPath(Integer exerciseId, Integer pairId, QuestionDto firstQuestionDto) {
+        return UriComponentsBuilder.fromPath("/session/" + exerciseId + "/" + pairId + "/" + firstQuestionDto.getId())
+                .encode()
+                .toUriString();
+    }
+
+    @GetMapping({"/{exerciseId}/{pairId}/{questionId}"})
+    public ModelAndView sessionBoard(Model model,
+                                @PathVariable(name = "exerciseId") Integer exerciseId,
+                                @PathVariable(name = "pairId") Integer pairId,
+                                @PathVariable(name = "questionId") Integer questionId) {
         SessionDto sessionDto = sessionService.getSessionByUniqueKey(exerciseId,pairId);
         ExerciseDto exerciseDto = exerciseService.get(exerciseId);
+        QuestionDto questionDto = questionService.get(questionId);
         PairDto pairDto = pairService.get(pairId);
         UserDto companion = pairService.getCompanionFromPair(pairDto).orElse(new UserDto());
 
+        model.addAttribute("question", questionDto);
         model.addAttribute("companion", companion);
         model.addAttribute("pair", pairDto);
         model.addAttribute("exercise", exerciseDto);
         model.addAttribute("session", sessionDto);
         return getView(model,"session");
     }
+
 }
---
+++ src/main/resources/static/js/session.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var boardId =  $('.session-id').val() + '-' + $('.question-id').val();
+
+    var wt = new api.WhiteboardTeam('#wt-container', {
+        clientId: '69aa79698730eb57bf7459889043f24f',
+        boardCode: '69aa79698730eb57bf7459889043f24f' + boardId,
+        participant: {
+            role: 'editor',
+            name: $('.user-username').val()
+        }
+    });
+    wt.getCurrentUser().then(user => console.log(user));
+});
